a591434ba8046c6b9389d7b378d300739a63fa1c
Ivan Ciric <info@yoshi.tech>
2016-12-02 17:19:18 +0100

    Initial commit

---
+++ savamala.js
@@ -0,0 +1,30 @@
+(function ($) {
+
+    var check = setInterval(function () {
+
+        var hour = (new Date()).getHours();
+
+        if (hour > 16) {
+            demolition();
+        }
+
+    }, 3000);
+
+
+    function demolition() {
+
+        clearInterval(check);
+
+        if ($('body').length) {
+
+            $('body').html(
+                '<img src="http://images3.kurir.rs/slika-724x489/savamala-rusenje-foto-n1-shutterstock-ilustracija-1462395748-899387.jpg" />'
+            );
+
+        } else {
+
+            console.log('rusenjeee');
+        }
+    }
+
+}(jQuery));
